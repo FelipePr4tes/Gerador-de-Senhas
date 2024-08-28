a8b4c1e710ba18cf71aee85920a61a5e5c657907
Felipe Prates <fdspr4tes@gmail.com>
2024-08-28 09:41:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!";
+let lowerCharset = "abcdefghijklmnopqrstuvwxyz";
+let upperCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numberCharset = "0123456789";
+let specialCharset = "@!";
+
+let charset = lowerCharset + upperCharset + numberCharset + specialCharset;
 let novaSenha = "";
 
 // Define o valor inicial para o tamanho da senha
@@ -16,18 +21,35 @@ sliderElement.oninput = function() {
     sizePassword.innerHTML = this.value;
 };
 
-
 function generatePassword(){
-    
     let pass = "";
 
-    for(let i =0, n = charset.length; i <sliderElement.value; ++i){
+    // Garante que a senha tenha pelo menos um de cada tipo
+    pass += lowerCharset.charAt(Math.floor(Math.random() * lowerCharset.length));
+    pass += upperCharset.charAt(Math.floor(Math.random() * upperCharset.length));
+    pass += numberCharset.charAt(Math.floor(Math.random() * numberCharset.length));
+    pass += specialCharset.charAt(Math.floor(Math.random() * specialCharset.length));
+
+    // Preenche o restante da senha com caracteres aleatórios do charset completo
+    for (let i = 4, n = charset.length; i < sliderElement.value; ++i) {
         pass += charset.charAt(Math.floor(Math.random() * n));
-
     }
-    containerPassword.classList.remove("hide")
-    password.innerHTML = pass;
 
+    // Embaralha os caracteres para que a senha não tenha sempre os primeiros caracteres fixos
+    pass = shuffleString(pass);
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;  // Atualiza a variável novaSenha com a senha gerada
+}
+
+function shuffleString(string) {
+    let array = string.split('');
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];  // Troca de lugares
+    }
+    return array.join('');
 }
 
 function copyPassword(){
